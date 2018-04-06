a4d531f56e5edc0ae57abade8c6874032ee74ac2
Maksim Chekrishov <maksim.chekrishov@gmail.com>
2018-04-06 11:54:04 +0300

    Try ths!

--- src/index.js
+++ src/index.js
@@ -1,2 +1,37 @@
 import {Centrifuge} from './centrifuge.js';
+
 export default Centrifuge;
+
+import {
+  ProtobufEncoder,
+  ProtobufDecoder,
+  methodType,
+  messageType
+} from './protobuf';
+
+import {
+  JsonEncoder,
+  JsonDecoder
+} from './json';
+
+class CentrifugeProtobuf extends Centrifuge {
+
+  _setFormat(format) {
+    if (format === 'protobuf') {
+      this._binary = true;
+      this._methodType = methodType;
+      this._messageType = messageType;
+      this._encoder = new ProtobufEncoder();
+      this._decoder = new ProtobufDecoder();
+    } else {
+      this._encoder = new JsonEncoder();
+      this._decoder = new JsonDecoder();
+    }
+  }
+}
+
+export CentrifugeProtobuf;
+
+
+import Centrifuge from 'centrifuge';
+import {CentrifugeProtobuf} from 'centrifuge';
